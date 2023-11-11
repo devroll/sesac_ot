3fcfa81dfb968b87f44ae0a002da62d681b934dc
heungje <heungje@namu-ai.com>
2023-11-11 17:39:38 +0900

    JS CRM 미니프로젝트

--- 2.JS/5.module/5.input/userinput.js
+++ 2.JS/5.module/5.input/userinput.js
@@ -10,16 +10,16 @@ const rl = readline.createInterface({
 rl.question('구구단의 단을 입력하세요:', (input) => {
     const num = parseInt(input);
 
-    if(!isNaN(num) && num > 0 && num < 10){
+    if (!isNaN(num) && num > 0 && num < 10) {
         console.log(`${num} 단 구구단을 출력합니다.`);
         // 이 단의 구구단을 출력하시오. 3줄 이내로 작성
-        for(let i = 1 ; i < 10 ; i++){
+        for (let i = 1; i < 10; i++) {
             let res = num * i;
             console.log(`${num} x ${i} = ${res}`);
         }
     } else {
-        consolel.log(`1 ~ 9 까지의 숫자를 입력하세여!!`)
-    }    
+        console.log(`1 ~ 9 까지의 숫자를 입력하세여!!`)
+    }
     rl.close();
 });
 
