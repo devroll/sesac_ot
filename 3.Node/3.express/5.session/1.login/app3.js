e010d6482ab587200cc1ec7acf427f836ed9a023
heungje <heungje@namu-ai.com>
2023-11-23 13:35:37 +0900

    로그인

--- 3.Node/3.express/5.session/1.login/app3.js
+++ 3.Node/3.express/5.session/1.login/app3.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const fs = require('fs');
+// const fs = require('fs');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const path = require('path');
---
+++ 3.Node/3.express/5.session/4.cart/public/static/cart.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('/products')
+        .then((response) => response.json())
+        .then((products) => displayProduct(products));
+});
+
+function displayProduct(products) {
+    // console.log(products);
+    const productTabledBody = document.querySelector('#productTable tbody');
+    console.log(productTabledBody);
+
+    products.forEach((product) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${product.id}</td>
+            <td>${product.name}</td>
+            <td>${product.price}</td>            
+            <td><button onclick="addToCart(${product.id})">담기</button></td>            
+        `;
+        productTabledBody.appendChild(row);
+    });
+}
+
+function addToCart(productId) {
+    // fetch(`/add-to-cart/${productId}`, { method: 'GET' })
+    fetch(`/add-to-cart/${productId}`, { method: 'POST' })
+        .then((response) => response.json())
+        .then((data) => {
+            alert(data.message);
+            // alert(JSON.stringify(data.cart));
+            fetch('/cart')
+                .then((response) => response.json())
+                .then((cart) => displayCart(cart));
+        });
+}
+
+function displayCart(cart) {
+    const cartTableBody = document.querySelector('#cartTable tbody');
+    cartTableBody.innerHTML = ''; // 지우고 새로 그림
+    cart.forEach((item) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${item.id}</td>
+            <td>${item.name}</td>
+            <td>${item.price}</td>
+        `
+        cartTableBody.appendChild(row);
+    });
+}
